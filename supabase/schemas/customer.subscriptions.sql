ee2fabccf72a406d457e5d5272b5e1c71e017fc2
danie <github@roux.one>
2026-04-14 16:26:41 +1200

    Add copyright file headers

--- supabase/schemas/customer.subscriptions.sql
+++ supabase/schemas/customer.subscriptions.sql
@@ -1,4 +1,17 @@
--- TODO: Make sure no weird grants exist on subscription
+-- Copyright (C) 2026 aisleron.com
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 create table customer.subscriptions (
   user_id uuid primary key references auth.users (id) on delete cascade,
